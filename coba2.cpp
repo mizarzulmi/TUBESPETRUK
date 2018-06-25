c6d22200e50e3a9ef9cfb35c9b58bfce0d74ae3e
Yosua Sandi Susanto <38531813+yosuasandi1064@users.noreply.github.com>
2018-06-25 08:34:34 +0700

    Update coba2.cpp

--- coba2.cpp
+++ coba2.cpp
@@ -4,115 +4,111 @@
 #include <string.h>
 #include <windows.h>
 using namespace std;
-struct tabel
-{
-char nama[20];
-char nilai[7];
+
+struct tabel{
+	char nama[20];
+	char nilai[7];
 }
+
 tbl[10];
-int n,xnyamug,y,a,z,closemu=0;
-char tamp[20],carigie[20];
-void garis()
-{
-cout<<"-------------------------------------------------------\n";
+int n,x,y,a,z,closemu=0;
+char tmp[20],searchnama[20];
+
+void garis(){
+	cout<<"-------------------------------------------------------\n";
 }
-void block()
-{
-cout<<"ßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßßß\n";
+
+void block(){
+	cout<<"ÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸÃŸ\n";
 }
-void inputmug()
-{
+
+void inputmug(){
 // Tampilan Awal
-garis();
-cout<<"Mau Berapa Data : ";cin>>n;
-cin.ignore();
-garis();
-cout<<endl;
-for(xnyamug=1;xnyamug<=n;xnyamug++)
-{
-cout<<"Input Data ke-"<<xnyamug<<endl;
-cout<<"Nama Mahasiswa\t\t: ";
-gets(tbl[xnyamug].nama);
-cout<<"Nilai Mahasiswa\t\t: ";
-cin>>tbl[xnyamug].nilai;
-cin.ignore();
+	garis();
+	cout<<"Mau Berapa Data : ";cin>>n;
+	cin.ignore();
+	garis();
+	cout<<endl;
+	for(x=1;x<=n;x++){
+		cout<<"Input Data ke-"<<x<<endl;
+		cout<<"Nama Mahasiswa\t\t: ";
+		gets(tbl[x].nama);
+		cout<<"Nilai Mahasiswa\t\t: ";
+		cin>>tbl[x].nilai;
+		cin.ignore();
 
-garis();
-cout<<endl;
-}
-}
-void mugtampil()
-{
-system("cls");
-// Tabel Data Setelah diinputkan
-garis();block();
-cout<<"Data Nilai Mahasiswa"<<endl;
-garis();
-cout<<"\tNo."<<"\tNama"<<"\t\tNilai"<<endl;
-for(xnyamug=1;xnyamug<=n;xnyamug++)
-{
-cout<<"\t"<<xnyamug<<"\t"<<tbl[xnyamug].nama<<"\t\t"<<tbl[xnyamug].nilai<<endl;
-}
-cout<<endl<<endl;
-}
-void mugsorting()
-{
-garis();block();
- cout<<"Sorting Data Secara Descending Berdasarkan Nilai"<<endl;
- garis();
-// Proses Selection Sort
- for(xnyamug=0;xnyamug<=n-1;xnyamug++)
- {
-  for(y=xnyamug+1;y<=n;y++)
-  {
-   a=strcmp(tbl[xnyamug].nilai,tbl[y].nilai);
-   if(a<0)
-   {
-   strcpy(tamp,tbl[xnyamug].nilai);
-   strcpy(tbl[xnyamug].nilai,tbl[y].nilai);
-   strcpy(tbl[y].nilai,tamp);
-   strcpy(tamp,tbl[xnyamug].nama);
-   strcpy(tbl[xnyamug].nama,tbl[y].nama);
-   strcpy(tbl[y].nama,tamp);
-   }
-  }
- }
-// Tabel Sorting Descending Berdasarkan Nilai
-cout<<"\tNo."<<"\tNilai"<<"\t\tNama"<<endl;
-for(y=0;y<=n-1;y++)
-{
-cout<<"\t"<<y+1<<"\t"<<tbl[y].nilai<<"\t\t"<<tbl[y].nama<<endl;
-}
-}
-void mugcari()
-{
-reset:
-cout<<endl;
-// Searching Data
-garis();block();
-cout<<"Terdapat 1 dari "<<n<<" Data Nilai Mahasiswa"<<endl;
-cout<<"Cari Nilai Berdasarkan Nama = ";cin>>carigie;
-garis();
-for(xnyamug=0;xnyamug<=n;xnyamug++)
- {
- if(strcmp(tbl[xnyamug].nama,carigie)==0)
- {
- cout<<"Nilai "<<carigie<<" Adalah\t : "<<tbl[xnyamug].nilai<<endl;
- cout<<"Terdapat di Peringkat \t : Ke-"<<xnyamug+1<<endl;
- closemu=1;
- }
- }
- if (closemu==0)
- {cout<<"Maaf, Data Nama "<<carigie<<" Tidak ditemukan, Coba Kembali :)";
- goto reset;}
-}
-int main()
-{
-system("cls");
-inputmug();
-mugtampil();
-mugsorting();
-mugcari();
-getch();
+		garis();
+		cout<<endl;
+		}
 }
 
+void mugtampil(){
+	system("cls");
+	// Tabel Data Setelah diinputkan
+	garis();
+	block();
+	cout<<"Data Nilai Mahasiswa Matakuliah "<<matkul[b]<<endl;
+	garis();
+	cout<<"\tNo."<<"\tNama"<<"\t\tNilai"<<endl;
+	for(x=1;x<=n;x++){
+		cout<<"\t"<<x<<"\t"<<tbl[x].nama<<"\t\t"<<tbl[x].nilai<<endl;
+	}
+	cout<<endl<<endl;
+}
+
+void mugsorting(){
+	garis();block();
+	cout<<"Sorting Data Secara Descending Berdasarkan Nilai"<<endl;
+	garis();
+	// Proses Selection Sort
+	for(x=0;x<=n-1;x++){
+		for(y=x+1;y<=n;y++){
+			a=strcmp(tbl[x].nilai,tbl[y].nilai);
+			if(a<0){
+				strcpy(tmp,tbl[x].nilai);
+				strcpy(tbl[x].nilai,tbl[y].nilai);
+				strcpy(tbl[y].nilai,tmp);
+				strcpy(tmp,tbl[x].nama);
+				strcpy(tbl[x].nama,tbl[y].nama);
+				strcpy(tbl[y].nama,tmp);
+			}
+		}
+	}
+	
+	// Tabel Sorting Descending Berdasarkan Nilai
+	cout<<"\tNo."<<"\tNilai"<<"\t\tNama"<<endl;
+	for(y=0;y<=n-1;y++){
+		cout<<"\t"<<y+1<<"\t"<<tbl[y].nilai<<"\t\t"<<tbl[y].nama<<endl;
+	}
+}
+
+void mugcari(){
+	reset:
+	cout<<endl;
+	// Searching Data
+	garis();
+	block();
+	cout<<"Terdapat 1 dari "<<n<<" Data Nilai Mahasiswa"<<endl;
+	cout<<"Cari Nilai Berdasarkan Nama = ";cin>>searchnama;
+	garis();
+	for(x=0;x<=n;x++){
+		if(strcmp(tbl[x].nama,searchnama)==0){
+			cout<<"Nilai "<<searchnama<<" Adalah\t : "<<tbl[x].nilai<<endl;
+			cout<<"Terdapat di Peringkat \t : Ke-"<<x+1<<endl;
+			closemu=1;
+		}
+	}
+	if (closemu==0)
+		{cout<<"Maaf, Data Nama "<<searchnama<" Tidak ditemukan, Coba Kembali :)";
+		goto reset;
+	}
+}
+
+int main(){
+	system("cls");
+	inputmug();
+	mugtampil();
+	mugsorting();
+	mugcari();
+	getch();
+}
